710d0f2e262ec9aa33313171624424f261396474
Huseyin <huseyineskan01@gmail.com>
2025-04-29 22:13:39 +0200

    added background image url input and it works.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
-import Navbar from "./components/Navbar";
 import CanvasDisplay from "./components/CanvasDisplay";
-import TextControl from "./components/TextControl";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
       <h1>PostForge</h1>
-
-      {/* <TextControl /> */}
       <CanvasDisplay />
     </>
   );
--- src/components/CanvasDisplay.jsx
+++ src/components/CanvasDisplay.jsx
@@ -1,27 +1,47 @@
 import React, { useRef, useEffect, useState } from "react";
 import "../styles/Canvas.css";
+import "../styles/TextControl.css";
 
 function CanvasDisplay() {
   const canvasRef = useRef(null);
 
   const [userText, setUserText] = useState("");
-  const [canvasWidth, setCanvasWidth] = useState(400);
-  const [canvasHeight, setCanvasHeight] = useState(400);
+  const [bgImageURL, setBgImageURL] = useState("");
+  const canvasWidth = 500;
+  const canvasHeight = 500;
 
   useEffect(() => {}, []);
 
   function createPost() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    const metrics = ctx.measureText(userText);
 
-    const textHorizontalCenter = (canvas.width - metrics.width) / 2;
-    const textVerticalCenter =
-      (canvas.height - metrics.actualBoundingBoxDescent) / 2;
+    const image = new Image();
+    image.crossOrigin = "anonymous";
+    image.src = bgImageURL;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillText(userText, textHorizontalCenter, textVerticalCenter);
-    ctx.font = "bold 40px system-ui";
+    image.onload = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // draw bg image
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      // Text styles
+      ctx.font = "bold 40px system-ui";
+      ctx.fillStyle = "#fff";
+
+      // Text position - center
+      const metrics = ctx.measureText(userText);
+      const textHorizontalCenter = (canvas.width - metrics.width) / 2;
+      const textVerticalCenter =
+        (canvas.height - metrics.actualBoundingBoxDescent) / 2;
+
+      ctx.fillText(userText, textHorizontalCenter, textVerticalCenter);
+    };
+
+    image.onerror = () => {
+      alert("Image could not be loaded. Please enter a valid image URL.");
+    };
   }
 
   return (
@@ -34,6 +54,15 @@ function CanvasDisplay() {
                 type="text"
                 placeholder="Text"
                 onChange={(e) => setUserText(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-input">
+              <input
+                type="text"
+                placeholder="Background image URL"
+                onChange={(e) => setBgImageURL(e.target.value)}
               />
             </div>
           </div>
